aa0f808425dd856ee35f64c058c46ae597f63691
Jaclyn Hall <jaclynchall@gmail.com>
2018-01-09 01:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-function destructivelyAppendKitten(kittens, kittenString) {
+function destructivelyAppendKitten(kittenString) {
   kittens.push(kittenString)
   return kittens
 }
